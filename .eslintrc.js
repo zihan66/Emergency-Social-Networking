e9e62860384435ffbd315204b1c2b58543797b4e
Zihan Wei <zihan66@tamu.edu>
2022-02-04 12:25:28 -0800

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    quotes: [2, "double", { avoidEscape: true }],
+  },
+};
