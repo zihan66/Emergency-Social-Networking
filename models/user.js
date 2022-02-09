891b4704ee47154a481a56a11f8047ff61489550
pinzhini <97963187+pinzhini@users.noreply.github.com>
2022-02-08 20:41:39 -0800

    updated user.js

--- models/user.js
+++ models/user.js
@@ -1,11 +1,10 @@
-class User {
-  constructor(username) {
-    this.username = username;
-  }
+const mongoose = require("mongoose");
 
-  save() {
-    /* Persist user info */
-  }
-}
+// establish database connection
+const userSchema = new mongoose.Schema({
+  username: { type: String, default: "", trim: true }, // trim: true will remove all leading and trailing spaces
+  password: { type: String, default: "", trim: true }, // trim: true will remove all leading and trailing spaces
+});
 
-module.exports = User;
+// current user
+module.exports = { userSchema };
