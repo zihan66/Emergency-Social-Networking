b904548533e9652741ad5cf24197f9cf35caedfd
Zihan Wei <zihan66@tamu.edu>
2022-02-08 21:07:48 -0800

    fix route

--- routes/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-  username: { type: String, default: "", trim: true },
-  password: { type: String, default: "", trim: true },
-});
-
-module.exports = { userSchema };
--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,16 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("login", { title: "Login" });
+  res.render("index", { title: "Login" });
 });
 
-router.get("/register", (req, res) => {
-  res.render("register", { title: "Register" });
+router.get("/signUp", (req, res) => {
+  res.render("signUp", { title: "SignUp" });
 });
+
+// router.get("/register", (req, res) => {
+//   res.render("register", { title: "Register" });
+// });
 
 router.get("/home", (req, res) => {
   res.render("home", { title: "Home" });
@@ -31,29 +35,46 @@ router.post("/login", async (req, res) => {
   console.log("req: POST /login", user);
   try {
     if (!user.username || !user.password) {
-      res.status(200).send({ status: "error", message: "username or password is empty" });
+      res
+        .status(200)
+        .send({ status: "error", message: "username or password is empty" });
       return;
     }
     if (user.username.length < 4) {
-      res.status(200).send({ status: "error", message: "username too short, should be at least 4 characters long." });
+      res.status(200).send({
+        status: "error",
+        message: "username too short, should be at least 4 characters long.",
+      });
       return;
     }
     if (resevedUsernameList.includes(user.username)) {
-      res.status(200).send({ status: "error", message: "username is reserved, please choose another one." });
+      res.status(200).send({
+        status: "error",
+        message: "username is reserved, please choose another one.",
+      });
       return;
     }
     user.username = user.username.toLowerCase();
     const result = await User.findOne(user);
     if (result) {
-      jwt.sign({ username: user.username }, "secret", { expiresIn: "1h" }, (err, token) => {
-        if (err) {
-          res.status(200).send({ status: "error", message: "internal error" });
-          return;
+      jwt.sign(
+        { username: user.username },
+        "secret",
+        { expiresIn: "1h" },
+        (err, token) => {
+          if (err) {
+            res
+              .status(200)
+              .send({ status: "error", message: "internal error" });
+            return;
+          }
+          res.status(200).send({ status: "success", token });
         }
-        res.status(200).send({ status: "success", token });
-      });
+      );
     } else {
-      res.status(200).send({ status: "error", message: "username or password is wrong" });
+      res
+        .status(200)
+        .send({ status: "error", message: "username or password is wrong" });
     }
   } catch (e) {
     res.status(200).send({ status: "error", message: e.message });
@@ -66,25 +87,39 @@ router.post("/register", async (req, res) => {
   console.log("req: POST /register", user);
   try {
     if (!user.username || !user.password) {
-      res.status(200).send({ status: "error", message: "username or password is empty" });
+      res
+        .status(200)
+        .send({ status: "error", message: "username or password is empty" });
       return;
     }
     if (user.username.length < 4) {
-      res.status(200).send({ status: "error", message: "username too short, should be at least 4 characters long." });
+      res.status(200).send({
+        status: "error",
+        message: "username too short, should be at least 4 characters long.",
+      });
       return;
     }
     if (resevedUsernameList.includes(user.username)) {
-      res.status(200).send({ status: "error", message: "username is reserved, please choose another one." });
+      res.status(200).send({
+        status: "error",
+        message: "username is reserved, please choose another one.",
+      });
       return;
     }
     if (user.password.length < 4) {
-      res.status(200).send({ status: "error", message: "password too short, should be at least 4 characters long." });
+      res.status(200).send({
+        status: "error",
+        message: "password too short, should be at least 4 characters long.",
+      });
       return;
     }
     user.username = user.username.toLowerCase();
     const one = await User.find({ username: user.username });
+    console.log(one);
     if (one.length > 0) {
-      res.status(200).send({ status: "error", message: "username already exists" });
+      res
+        .status(200)
+        .send({ status: "error", message: "username already exists" });
       return;
     }
     const result = await User.create(user);
