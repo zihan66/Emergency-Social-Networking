fc0b9e0235ffcc5c2aa345d3a9602f1a91bbbd12
Zihan Wei <zihan66@tamu.edu>
2022-02-15 13:30:37 -0800

    add modal to confirm joining

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -603,26 +603,32 @@ const reservedUsernameList = [
 ];
 
 const { cookies } = brownies;
-const joinCommunity = document.querySelector("button");
-joinCommunity.addEventListener("click", async (e) => {
+const joinCommunity = document.querySelector(".button");
+// Get the modal
+const modal = document.querySelector(".modal-wrapper");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+const confirmButton = document.querySelector(".confirm");
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
+
+confirmButton.addEventListener("click", async (e) => {
   e.preventDefault();
   const username = document.forms[0].querySelectorAll("input")[0].value;
-  if (username.length < 3) {
-    const ele = document.querySelector("#username-hint");
-    ele.innerHTML = "Username should have minimum four characters";
-    return;
-  }
-  if (reservedUsernameList.includes(username)) {
-    const ele = document.querySelector("#username-hint");
-    ele.innerHTML = "Username is reserved, please choose another one";
-    return;
-  }
   const password = document.forms[0].querySelectorAll("input")[1].value;
-  if (password.length < 4) {
-    const ele = document.querySelector("#password-hint");
-    ele.innerHTML = "Password should have minimum four characters";
-    return;
-  }
+  modal.style.display = "none";
   const data = { username, password };
   try {
     let response = await fetch("/register", {
@@ -639,17 +645,37 @@ joinCommunity.addEventListener("click", async (e) => {
       ele.innerHTML = response.message;
       return;
     }
-
-    // const token = cookies.jwtToken;
-    // response = await fetch("/welcome", {
-    //   method: "get",
-    //   headers: {
-    //     Authorization: `Bearer ${token}`,
-    //   },
-    // });
-    // console.log(token);
     window.location.href = "/welcome";
   } catch (err) {
     console.error(err);
   }
+});
+
+joinCommunity.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const username = document.forms[0].querySelectorAll("input")[0].value;
+  const password = document.forms[0].querySelectorAll("input")[1].value;
+  if (username.length < 3) {
+    const ele = document.querySelector("#username-hint");
+    ele.innerHTML = "Username should have minimum three characters";
+    return;
+  }
+
+  if (reservedUsernameList.includes(username)) {
+    const ele = document.querySelector("#username-hint");
+    ele.innerHTML = "Username is reserved, please choose another one";
+    return;
+  }
+
+  const ele = document.querySelector("#username-hint");
+  ele.innerHTML = "";
+
+  if (password.length < 4) {
+    const eleP = document.querySelector("#password-hint");
+    eleP.innerHTML = "Password should have minimum four characters";
+    return;
+  }
+  const eleP = document.querySelector("#password-hint");
+  eleP.innerHTML = "";
+  modal.style.display = "block";
 });
