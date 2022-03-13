2790b35f4b46aa6347f1b210635fd716883b7729
Zihan Wei <zihan66@tamu.edu>
2022-03-12 18:06:41 -0800

    add test

---
+++ test/chatTest.test.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const Chat = require("../models/chat");
+let mongoServer;
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = await mongoServer.getUri();
+  const mongooseOpts = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  };
+  await mongoose.connect(uri, mongooseOpts);
+});
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
+
+test("It should be possible to save a new chat", async () => {
+  const hakanChat = new Chat({
+    username1: "Hakan",
+    username2: "Hakan2",
+    chatID: new Date().getTime().toString(36),
+  });
+  const response = await hakanChat.save();
+  const chat = await Chat.findOne({ username1: "Hakan" });
+  expect(chat.username2).toBe(hakanChat.username2);
+});
