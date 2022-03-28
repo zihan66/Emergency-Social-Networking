7ec8551a555b290e94f05933aed1405e4d7e95ca
pinzhini <pinzhin@andrew.cmu.edu>
2022-03-28 10:31:59 -0700

    api tests for join, login and logout

--- test/test-api-jest.js
+++ test/test-api-jest.js
@@ -38,8 +38,8 @@ afterAll(async () => {
 
 
 // Dummy Users
-const dummy = new User({userName: 'Arthur', password: 'vwy207', islogin: true, isAcknowledge: true});
-var goofy = { username: 'Ritvik', password: 'vwy207', status: Status.OK };
+const dummy = new User({ userName: 'Arthur', password: '123456', islogin: false, isAcknowledge: false });
+const goofy = new User({ username: 'Ritvik', password: 'abc123', islogin: false, isAcknowledge: false });
 var silly = { username: 'access', password: 'xyz124' };
 var folly = { username: 'Jane', password: 'aa' };
 
@@ -88,6 +88,54 @@ test("Can register a new user", (done) => {
 
 
 
+// try to login an existing user
+test("Can login a user", (done) => {
+    agent.put(HOST + '/users/dummy/online').send(dummy).end(function (err, res) {
+        expect(err).not.toBe(null);
+        expect(res.statusCode).toBe(200);
+
+        agent.get(HOST + '/users').send().end(function (err, res) {
+            let users = res.body;
+            let arthur = users.find((u) => u.userName === dummy.userName);
+            expect(arthur.islogin).toBe(true);
+            done();
+        })
+    })
+})
+
+
+
+
+// try to make acknowledgement for a user
+test("Can acknowledge a user", (done) => {
+    agent.put(HOST + '/users/dummy/acknowledgement').send().end(function (err, res) {
+        expect(res.statusCode).toBe(302);
+        done();
+    })
+})
+
+
+
+
+// user login and check online status
+test("Can get a user's online status", (done) => {
+    agent.put(HOST + '/users/dummy/online').send(dummy).end(function (err, res) {
+        expect(res.statusCode).toBe(200);
+
+        agent.get(HOST + 'users/dummy/online').send().end(function (err, res) {
+            let users = res.body;
+            let arthur = users.find((u) => u.userName === dummy.userName);
+            expect(arthur.islogin).toBe(dummy.islogin);
+            done();
+        });
+        
+    });
+
+});
+
+
+
+// try register with reserved username
 test('Should reject post with invalid username with right response code', (done) => {
     agent.post(HOST + '/users')
         .send(silly)
@@ -100,6 +148,7 @@ test('Should reject post with invalid username with right response code', (done)
 
 
 
+// try register with too short password
 test('Should reject post with invalid password with right response code', (done) => {
     agent.post(HOST + '/users')
         .send(folly)
@@ -110,6 +159,49 @@ test('Should reject post with invalid password with right response code', (done)
         });
 });
 
+
+
+// try to logout a user who was initially online
+test("Can logout a user", (done) => {
+    agent.put(HOST + '/users/dummy/offline').send().end(function (err, res) {
+        expect(res.statusCode).toBe(200);
+        done();
+    })
+
+});
+
+
+
+// try to retrieve all users
+test('Can retrieve all users', (done) => {
+    agent.post(HOST + '/users').send(goofy).end(function (err, res) {
+        expect(err).toBe(null);
+        expect(res.statusCode).toBe(201);
+
+        agent.get(HOST + '/users').send().end(function (err, res) {
+            expect(err).toBe(null);
+            expect(res.statusCode).toBe(200);
+
+            let users = res.body;
+            expect(users).toContainEqual({
+                username: 'Arthur',
+                password: '123456',
+                islogin: true,
+                isAcknowledge: true
+            });
+            expect(users).toContainEqual({
+                username: 'Ritvik',
+                password: 'abc123',
+                islogin: true,
+                isAcknowledge: true
+            });
+
+            done();
+        });
+
+    });
+
+});
 
 
 
