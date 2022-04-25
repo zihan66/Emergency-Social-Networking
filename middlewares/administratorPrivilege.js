6f97541c059d263f0dace0552256cc8626fb545c
Zihan Wei <zihan66@tamu.edu>
2022-04-24 20:45:10 -0700

    fix bug

--- middlewares/administratorPrivilege.js
+++ middlewares/administratorPrivilege.js
@@ -1,7 +1,7 @@
 const User = require("../models/user");
 const administratorPrivilege = (req, res, next) => {
   const username = req.cookies.username;
-  const user = User.findOne({ username: username });
+  const user = await User.findOne({ username: username });
   if (user.privilege === "administrator") {
     next();
   } else res.status(401).render("needToBeAdmin", { title: "needToBeAdmin" });
